e9b0f775db98be6cd39666a08a571a631eca28b9
Manuel Diaz <macnolo@192.168.1.2>
2024-07-28 23:06:07 -0600

    fix(themes): fix rose pine theme

--- lisp/themes/rose-pine-theme.el
+++ lisp/themes/rose-pine-theme.el
@@ -548,12 +548,5 @@
 
  ))
 
-;;;###autoload
-(and load-file-name
-     (boundp 'custom-theme-load-path)
-     (add-to-list 'custom-theme-load-path
-                  (file-name-as-directory
-                   (file-name-directory load-file-name))))
-
-(provide 'rose-pine-theme)
+(provide-theme 'rose-pine)
 ;;; rose-pine-theme.el ends here
