1ab962a7073ce92f66a339442c31f7c713c3ac66
Manuel Díaz <yosoymacnolo@gmail.com>
2025-06-21 15:01:12 -0600

    fix!: remove old major modes

--- lisp/m-lsp.el
+++ lisp/m-lsp.el
@@ -29,47 +29,8 @@
   :config
   (yas-global-mode 1))
 
-;; (use-package elixir-ts-mode
-;;   :config
-;;   (add-hook 'elixir-format-hook
-;;     (lambda () (if (projectile-project-p)
-;;                  (setq elixir-format-arguments
-;;                    (list "--dot-formatter"
-;;                      (concat (locate-dominating-file buffer-file-name ".formatter.exs") ".formatter.exs")))
-;;                  (setq elixir-format-arguments nil))))
-;;   (add-hook 'elixir-mode-hook
-;;     (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
-;;   (add-hook 'elixir-mode-hook 'eglot-ensure))
-
-(use-package
-  exunit
-  :diminish t
-  :bind
-  ("C-c e ." . exunit-verify-single)
-  ("C-c e b" . exunit-verify)
-  ("C-c e u a" . exunit-verify-all-in-umbrella)
-  ("C-c e a" . exunit-verify-all)
-  ("C-c e l" . exunit-rerun))
-
-(use-package typescript-mode
-  :mode "\\.ts\\'"
-  :config
-  (setq typescript-indent-level 2))
-
-;; (use-package js2-mode
-;;   :mode "\\.jsx?\\'"
-;;   :config
-;;   (add-to-list 'magic-mode-alist '("#!/usr/bin/env node" . js2-mode))
-;;   (setq js2-mode-show-strict-warnings nil)
-;;   (setq-default js-indent-level 2))
-
-(use-package web-mode
-  :mode "(\\.\\(html?\\|ejs\\|tsx\\|jsx\\)\\'"
-  :config
-  (setq-default web-mode-code-indent-offset 2)
-  (setq-default web-mode-markup-indent-offset 2)
-  (setq-default web-mode-attribute-indent-offset 2))
-
 (use-package clojure-ts-mode)
+
+(use-package elixir-ts-mode)
 
 (provide 'm-lsp)
